139b47aea9918fecfdb24f9416aeccaa27cb6b09
SquadTeemo <patrice_office@yahoo.com>
2022-05-20 18:53:46 +0100

    animation added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,29 @@
 import {Link} from "react-router-dom";
+import {useState} from "react";
 
 import "./Home.css";
 import Navigation from "../../components/Navigation/Navigation";
+import video from "../../assets/vids/space_ship.mp4";
+import {useEffect} from "react";
 
 const Home = () => {
+  const [play, setPlay] = useState(true);
+  const [statiq, setStatiq] = useState("home-container");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPlay(false);
+    }, 40000);
+  }, []);
+
+  useEffect(() => {
+    if (!play) {
+      setStatiq("home-container home-container-static");
+    }
+  }, [play]);
+
   return (
-    <div className="home-container">
+    <div className={statiq}>
       <Navigation />
       <div className="home-content">
         <div className="home-left">
@@ -22,6 +40,15 @@ const Home = () => {
           <Link to="/destination">EXPLORE</Link>
         </div>
       </div>
+      {play && (
+        <video
+          src={video}
+          autoPlay={true}
+          muted={true}
+          loop
+          id="myVideo"
+        ></video>
+      )}
       <p className="home-footer">Coded by @PatriceAm 2022</p>
     </div>
   );
